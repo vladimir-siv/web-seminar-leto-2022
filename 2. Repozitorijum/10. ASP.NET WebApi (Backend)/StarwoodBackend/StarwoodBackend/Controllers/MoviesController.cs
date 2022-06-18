aca5b5fd0662eeaf67b7c3df7dfe53192550c4cd
vladimir-siv <vladimir.sivcev@gmail.com>
2022-06-18 04:37:02 +0200

    Prezentacija 10 - bez modela

---
+++ 2. Repozitorijum/10. ASP.NET WebApi (Backend)/StarwoodBackend/StarwoodBackend/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StarwoodBackend.Controllers
+{
+    [Route("api/movies")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static readonly Random RNG = new Random();
+        private static readonly string[] Movies = new string[]
+        {
+            "The Fast & Furious",
+            "Avatar",
+            "The Hangover",
+            "Star Wars: A New Hope",
+            "The Lord of the Rings: The Two Towers",
+            "Forrest Gump",
+            "The Terminator",
+            "The Matrix",
+            "Shutter Island",
+            "Joker"
+        };
+
+        [HttpGet("most-popular")]
+        public string MostPopular()
+        {
+            return "The Shawshank Redemption";
+        }
+
+        [HttpGet("trending")]
+        public List<string> Trending(int? count = 4)
+        {
+            var trending = new List<string>((int)count);
+
+            for (var i = 0; i < count; ++i)
+            {
+                var movieIndex = RNG.Next(Movies.Length);
+                trending.Add(Movies[movieIndex]);
+            }
+
+            return trending;
+        }
+    }
+}
---
+++ 2. Repozitorijum/10. ASP.NET WebApi (Backend)/StarwoodBackend/StarwoodBackend/Controllers/MoviesV3Controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Xml;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StarwoodBackend.Controllers
+{
+    [Route("api/movies-v3")]
+    [ApiController]
+    public class MoviesV3Controller : ControllerBase
+    {
+        private static readonly Random RNG = new Random();
+
+        private static string[] ReadXMLMovies(string file)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(file);
+
+            XmlElement root = document.DocumentElement;
+
+            string[] movies = new string[root.ChildNodes.Count];
+
+            for (var i = 0; i < root.ChildNodes.Count; ++i)
+            {
+                var child = root.ChildNodes[i];
+                movies[i] = child.Attributes["name"].Value;
+            }
+
+            return movies;
+        }
+
+        [HttpGet("most-popular")]
+        public string MostPopular()
+        {
+            return "The Shawshank Redemption";
+        }
+
+        [HttpGet("trending")]
+        public List<string> Trending(int? count = 4)
+        {
+            var Movies = ReadXMLMovies(@"Resources\movies.xml");
+
+            var trending = new List<string>((int)count);
+
+            for (var i = 0; i < count; ++i)
+            {
+                var movieIndex = RNG.Next(Movies.Length);
+                trending.Add(Movies[movieIndex]);
+            }
+
+            return trending;
+        }
+    }
+}
