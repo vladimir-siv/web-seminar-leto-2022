37f702ff7ae46a2f804acb2add708fa1c7d4e6be
vladimir-siv <vladimir.sivcev@gmail.com>
2022-06-19 20:00:18 +0200

    Prezentacija 12 teorija

---
+++ 2. Repozitorijum/12. Entity Framework (ORM)/StarwoodBackend/StarwoodBackend/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace StarwoodBackend.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Genre> Genres { get; set; }
+    }
+}
